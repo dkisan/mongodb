975d93cba61f668aee9f6ce9bb90e5dd1ca6bb34
DebasishKisan1 <debasish.kisan@gmail.com>
2023-12-26 13:18:34 +0530

    crud

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, price, description)
+  const product = new Product(title, price, imageUrl, description)
   product.save()
     .then(result => {
       // console.log(result);
@@ -31,11 +31,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  req.user
-    .getProducts({ where: { id: prodId } })
-    // Product.findById(prodId)
-    .then(products => {
-      const product = products[0];
+  // req.user
+  //   .getProducts({ where: { id: prodId } })
+  Product.findById(prodId)
+    .then(product => {
       if (!product) {
         return res.redirect('/');
       }
@@ -55,14 +54,8 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findById(prodId)
-    .then(product => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
-      return product.save();
-    })
+  const product = new Product(updatedTitle, updatedPrice, updatedImageUrl, updatedDesc, prodId)
+  product.save()
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -71,8 +64,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  req.user
-    .getProducts()
+  Product.fetchAll()
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -85,10 +77,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId)
-    .then(product => {
-      return product.destroy();
-    })
+  Product.deleteById(prodId)
     .then(result => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
--- models/product.js
+++ models/product.js
@@ -1,17 +1,69 @@
 const getDb = require('../util/database').getDb;
+const mongodb = require('mongodb')
 
 class Product {
-  constructor(title, price, imageUrl, description) {
+  constructor(title, price, imageUrl, description, id) {
     this.title = title
     this.price = price
     this.imageUrl = imageUrl
     this.description = description
+    this._id = new mongodb.ObjectId(id)
   }
 
   save() {
     const db = getDb();
+    let dbOp;
+    if (this._id) {
+      dbOp = db.collection(process.env.db_collection).updateOne({ _id: this._id }, { $set: this })
+    } else {
+      dbOp = db.collection(process.env.db_collection)
+        .insertOne(this)
+    }
+    return dbOp
+      .then(result => {
+        console.log('added')
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
+  }
+
+  static fetchAll() {
+    const db = getDb();
     return db.collection(process.env.db_collection)
-      .insertOne(this)
+      .find()
+      .toArray()
+      .then(products => {
+        return products
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db.collection(process.env.db_collection)
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then(product => {
+        return product
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
+  }
+
+  static deleteById(prodId) {
+    const db = getDb()
+    return db.collection(process.env.db_collection)
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then(product => {
+        return product
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
   }
 }
 
