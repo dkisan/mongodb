463c89065fec30c31788df13d7f0a2f9be712ae2
DebasishKisan1 <debasish.kisan@gmail.com>
2023-12-27 17:56:22 +0530

    cart system

--- models/user.js
+++ models/user.js
@@ -15,7 +15,7 @@ const userSchema = new Schema({
       {
         productId: {
           type: Schema.Types.ObjectId,
-          ref:'Product',
+          ref: 'Product',
           required: true
         },
         quantity: {
@@ -26,6 +26,25 @@ const userSchema = new Schema({
     ]
   }
 })
+
+userSchema.methods.addToCart = function (product) {
+  const cartProductIndex = this.cart.items.findIndex(cp => {
+    return cp.productId.toString() === product._id.toString()
+  })
+  let newQuantity = 1
+  let updatedCartItems = [...this.cart.items]
+  if (cartProductIndex >= 0) {
+    newQuantity = updatedCartItems[cartProductIndex].quantity + 1
+    updatedCartItems[cartProductIndex].quantity = newQuantity
+  } else {
+    updatedCartItems.push({ productId: product._id, quantity: 1 })
+  }
+  const updatedCart = {
+    items: updatedCartItems
+  }
+  this.cart = updatedCart;
+  return this.save()
+}
 
 module.exports = mongoose.model('User', userSchema)
 
